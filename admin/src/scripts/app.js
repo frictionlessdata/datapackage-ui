34aa4caba45a8f11ccfc98cbd26d386dc69abec2
nide <nenegoro@gmail.com>
2015-05-19 01:06:18 +0300

    Removed UI testing component

--- admin/src/scripts/app.js
+++ admin/src/scripts/app.js
@@ -2,7 +2,6 @@ var backbone = require('backbone');
 var BigbangView = require('./components/ui/bigbang');
 var registry = require('./components/datapackage-registry');
 var Router = require('./router');
-var ok = require('./components/ui/ok.js');
 var dataPackageProfiles = registry.get();
 
 
@@ -11,9 +10,6 @@ window.ROUTER = new Router();
 
 window.APP.render();
 backbone.history.start({pushState: true});
-
-// the logic for the Open Knowledge banner
-ok();
 
 // A simple example of the datapackages-registry lib
 // is given in the registry.get() method (see console).
