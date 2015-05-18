dabbd86d35ff8aa669d696f2a991b42f3f6e5b53
nide <nenegoro@gmail.com>
2015-05-19 00:51:12 +0300

    Extend UI app witn navigation bar

--- admin/src/scripts/app.js
+++ admin/src/scripts/app.js
@@ -1,14 +1,15 @@
 var backbone = require('backbone');
-var backboneBase = require('backbone-base');
+var BigbangView = require('./components/ui/bigbang');
 var registry = require('./components/datapackage-registry');
 var Router = require('./router');
 var ok = require('./components/ui/ok.js');
 var dataPackageProfiles = registry.get();
 
 
-window.APP = new backbone.BaseView({el: '#application'});
+window.APP = new BigbangView({el: '#application'});
 window.ROUTER = new Router();
 
+window.APP.render();
 backbone.history.start({pushState: true});
 
 // the logic for the Open Knowledge banner
---
+++ admin/src/scripts/components/ui/navbar.js
@@ -0,0 +1,5 @@
+var backbone = require('backbone');
+var backboneBase = require('backbone-base');
+
+
+module.exports = backbone.BaseView;
