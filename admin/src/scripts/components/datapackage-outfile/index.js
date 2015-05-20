c838358109ce8a004b0bcaf9755cbb71b89df89f
nide <nenegoro@gmail.com>
2015-05-20 13:17:59 +0300

    Object/emptyness validation tests passed (#7)

--- admin/src/scripts/components/datapackage-outfile/index.js
+++ admin/src/scripts/components/datapackage-outfile/index.js
@@ -1,7 +1,13 @@
+var _ = require('underscore');
+
+
 /**
  * Get datapackage descriptor and return object that will be used to describe 
  * json file for download.
  */
 module.exports = function(descriptor) {
+  if (_.isEmpty(descriptor) || !_.isObject(descriptor))
+    throw new Error('Non-empty descriptor object should be passed');
+
   return {}
 };
--- admin/src/scripts/components/datapackage-outfile/test.js
+++ admin/src/scripts/components/datapackage-outfile/test.js
@@ -4,14 +4,34 @@ var outfile = require('../datapackage-outfile');
 var should = require('chai').should();
 
 describe('Data Package Output File', function() {
-  it('throw error if empty descriptor passed', function() {
-    assert.equal(false, true);
-    done();
+  it('throw error if empty descriptor passed', function(done, err) {
+    if (err) done(err);
+
+    try {
+      outfile({});
+    } catch(exception) {
+      exception.message.should.be.not.empty;
+      exception.message.should.be.a('string');
+      done();
+      return;
+    }
+
+    done('Exception not thrown');
   });
 
-  it('throw error if non-object passed as a descriptor', function() {
-    assert.equal(false, true);
-    done();
+  it('throw error if non-object passed as a descriptor', function(done, err) {
+    if (err) done(err);
+
+    try {
+      outfile(123);
+    } catch(exception) {
+      exception.message.should.be.not.empty;
+      exception.message.should.be.a('string');
+      done();
+      return;
+    }
+
+    done('Exception not thrown');
   });
 
   it('throw error if required properties are missed in a descriptor', function() {
