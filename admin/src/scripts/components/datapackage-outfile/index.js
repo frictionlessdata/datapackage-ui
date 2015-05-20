11508ab49e40851c51fcebbc83602c3dbeb9cf87
nide <nenegoro@gmail.com>
2015-05-20 13:18:15 +0300

    Added missed semicolon

--- admin/src/scripts/components/datapackage-outfile/index.js
+++ admin/src/scripts/components/datapackage-outfile/index.js
@@ -9,5 +9,5 @@ module.exports = function(descriptor) {
   if (_.isEmpty(descriptor) || !_.isObject(descriptor))
     throw new Error('Non-empty descriptor object should be passed');
 
-  return {}
+  return {};
 };
