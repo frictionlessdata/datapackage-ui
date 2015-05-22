1114068f99a21dbd04adcbcaa4905ea2c1a952a0
nide <nenegoro@gmail.com>
2015-05-22 15:21:08 +0300

    Added confirmation dialog empty test cases (#10)

---
+++ admin/src/scripts/components/ui/test.js
@@ -0,0 +1,18 @@
+window = {};
+
+var _ = require('underscore');
+var assert = require('chai').assert;
+var should = require('chai').should();
+
+
+describe('UI Confirmation Dialog box', function() {
+  it('define Yes/No event handlers', function(done, err) {
+    if (err) done(err);
+    assert.fail();
+  });
+
+  it('call .yes/.no callbacks in Yes/No event handlers', function(done, err) {
+    if (err) done(err);
+    assert.fail();
+  });
+});
