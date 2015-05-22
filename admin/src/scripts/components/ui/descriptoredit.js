90691013e3966424f17fdb52f876fb4bc1fba8e7
nide <nenegoro@gmail.com>
2015-05-22 13:08:50 +0300

    Avoid extra indentation

--- admin/src/scripts/components/ui/descriptoredit.js
+++ admin/src/scripts/components/ui/descriptoredit.js
@@ -6,17 +6,18 @@ module.exports = {
   DescriptorEditView: backbone.BaseView.extend({
     render: function() {
       // JSON editor can not be created without json schema
-      if(this.schema) {
-        // If editor already exists destroy if
-        if(this.layout.form)
-          this.layout.form.destroy();
+      if(!this.schema)
+        return this;
 
-        this.layout.form = new JSONEditor(this.el, {
-          ajax: true,
-          schema: this.schema,
-          theme: 'bootstrap3'
-        });
-      }
+      // If editor already exists destroy if
+      if(this.layout.form)
+        this.layout.form.destroy();
+
+      this.layout.form = new JSONEditor(this.el, {
+        ajax: true,
+        schema: this.schema,
+        theme: 'bootstrap3'
+      });
 
       return this;
     },
