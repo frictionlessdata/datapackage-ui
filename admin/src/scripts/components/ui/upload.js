52048242bc8e92410c7093fd190214e9ea69d2a0
nide <nenegoro@gmail.com>
2015-05-22 12:11:42 +0300

    Show errors whil uploading data-package.json (#8)

--- admin/src/scripts/components/ui/upload.js
+++ admin/src/scripts/components/ui/upload.js
@@ -7,19 +7,26 @@ var backboneBase = require('backbone-base');
 
 module.exports = backbone.BaseView.extend({
   events: {  
-    'change': function(E) {
+    'change [data-id=input]': function(E) {
+      this.setError(null);
+
       FileAPI.readAsText(FileAPI.getFiles(E.currentTarget)[0], (function (EV) {
         if(EV.type === 'load') {
-          // Throw uploaded data into the wild
-          this.trigger('upload', JSON.parse(EV.result));
+          try {
+            // Throw uploaded data into the wild
+            this.trigger('upload', JSON.parse(EV.result)); 
+          } catch(exception) {
+            this.setError(exception.message);
+          }
         } else if( EV.type ==='progress' ){
           this.setProgress(EV.loaded/EV.total * 100);
         } else {
-          // WARN Throw error
+          this.setError('File upload failed');
         }
       }).bind(this))
     }
   },
 
+  setError: function(message) { this.$('[data-id=error]').html(message || ''); return this; },
   setProgress: function(percents) { return this; }
 });
