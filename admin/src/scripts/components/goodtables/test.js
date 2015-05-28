b5905d05dd1896c43d0156fb9ee44b8c13952399
nide <nenegoro@gmail.com>
2015-05-28 14:36:56 +0300

    Added goodtables module with empty test cases (#32)

---
+++ admin/src/scripts/components/goodtables/test.js
@@ -0,0 +1,26 @@
+var _ = require('underscore');
+var assert = require('chai').assert;
+var should = require('chai').should();
+
+
+describe('Goodtables API wrapper', function() {
+  it('respect passed param for request method', function(done, err) {
+    if(err) done(err);
+    assert(false);
+  });
+
+  it('provide default values for all params', function(done, err) {
+    if(err) done(err);
+    assert(false);
+  });
+
+  it('return Promise object', function(done, err) {
+    if(err) done(err);
+    assert(false);
+  });
+
+  it('reject with a message when connection failed', function(done, err){
+    if(err) done(err);
+    assert(false);
+  });
+});
