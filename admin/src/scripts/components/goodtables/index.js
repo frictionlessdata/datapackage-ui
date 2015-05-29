1ef10ae49f93a4f77c2963dc3f1e255e4a6368ec
nide <nenegoro@gmail.com>
2015-05-29 14:44:27 +0300

    Runflow comments

--- admin/src/scripts/components/goodtables/index.js
+++ admin/src/scripts/components/goodtables/index.js
@@ -5,6 +5,7 @@ var request = require('superagent');
 
 
 module.exports = function(options) {
+  // Provide default values for most params
   this.options = _.extend({
     fail_fast        : true,
     format           : 'csv',
@@ -17,12 +18,15 @@ module.exports = function(options) {
   if(!_.contains(['get', 'post'], this.options.method))
     throw new Error('.method should be "get" or "post"');
 
+  // /api/run — Validation Runner
   this.run = (function(data, schema) {
     if(!data)
       throw new Error('You need to provide data file to validate');
 
     return new Promise((function(RS, RJ) {
       request[this.options.method](API_URL + 'run')
+
+        // Provide request data with .query() in case of GET, otherwise use .send()
         [this.options.method == 'get' ? 'query' : 'send'](_.omit(this.options, 'method'))
 
         .end(function(E, R) {
