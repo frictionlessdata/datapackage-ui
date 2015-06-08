d4e9995b5122657cb79ba3a54eb359898e411c4e
nide <nenegoro@gmail.com>
2015-06-08 13:24:32 +0300

    All navigation views should be written in navigation.js (#45)

---
+++ admin/src/scripts/components/ui/navigation.js
@@ -0,0 +1,19 @@
+var backbone = require('backbone');
+var backboneBase = require('backbone-base');
+
+
+module.exports = {
+  NavbarView: backbone.BaseView.extend({
+    events: {
+      'click [data-id=badge]': function() {
+        this.toggleBadge();
+        return false;
+      }
+    },
+
+    toggleBadge: function(state) {
+      this.$('[data-id=badge]').toggleClass('collapsed', state);
+      return this;
+    }
+  })
+};
