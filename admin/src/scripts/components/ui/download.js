4429baab58692bbd7c88b58f8b2a95cc0a02cafb
nide <nenegoro@gmail.com>
2015-06-25 14:19:07 +0300

    Refined datapackage download view to work with Promised datapavkage-validate (#27)

--- admin/src/scripts/components/ui/download.js
+++ admin/src/scripts/components/ui/download.js
@@ -5,24 +5,20 @@ var validator = require('datapackage-validate');
 
 
 module.exports = backbone.BaseView.extend({
-  events: {
-    'click': function() {
-      window.APP.layout.errorList.clear();
-
-      var
-        validateResult = validator.validate(window.APP.layout.descriptorEdit.layout.form.getValue());
-
-      if(!validateResult.valid)
-        window.APP.layout.errorList.reset(new backbone.Collection(validateResult.errors));
-
-      return validateResult.valid;
-    }
-  },
-
   reset: function(descriptor) {
-    this.$el.attr('href', outfile(descriptor, {
-      IE9: window.APP.browser.name == 'ie' && parseInt(window.APP.browser.version.split('.')[0]) <= 9
-    }));
+    this.$el.addClass('disabled');
+
+    validator.validate(window.APP.layout.descriptorEdit.layout.form.getValue()).then((function(R) {
+      if(R.valid)
+        this.$el
+          .removeClass('disabled')
+
+          .attr('href', outfile(descriptor, {
+            IE9: window.APP.browser.name == 'ie' && parseInt(window.APP.browser.version.split('.')[0]) <= 9
+          }));
+
+      window.APP.layout.errorList.reset(new backbone.Collection(R.errors));
+    }).bind(this));
 
     return this;
   }
