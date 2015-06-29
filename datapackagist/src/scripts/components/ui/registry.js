c04dc94aa91244c7de551fe3aff6f6ff076bc8dc
nide <nenegoro@gmail.com>
2015-06-29 15:58:34 +0300

    Setting up registry value should be less conditional if it is done not by user (#57)

--- datapackagist/src/scripts/components/ui/registry.js
+++ datapackagist/src/scripts/components/ui/registry.js
@@ -7,7 +7,45 @@ var deep = require('deep-diff');
 module.exports = {
   ListView: backbone.BaseListView.extend({
     events: {
-      'change': function(event) { this.setSelectedSchema(this.$(this.options.container).val()); }
+      'change': function(event) {
+        var id = this.$(this.options.container).val();
+
+
+        $.getJSON(this.collection.get(id).get('schema'), (function(schemaData) {
+          var
+            // Keys of entered fields
+            keys = _.keys(this.parent.getFilledValues());
+
+          this.schemaData = schemaData;
+
+          // If data can be lost - show confirmation message
+          if(deep.diff(_.pick(schemaData.properties, keys), _.pick(this.parent.layout.form.schema.properties, keys)))
+            return window.APP.layout.confirmationDialog
+              .setMessage('Some data you have entered will be lost. Are you sure you want to change the Data Package Profile?')
+
+              .setCallbacks({
+                yes: (function() {
+
+                  this.selectedValue = id;
+                  this.parent.reset(schemaData);
+                  window.APP.layout.confirmationDialog.deactivate();
+                }).bind(this),
+
+                no: (function() {
+                  this.$(this.options.container).val(this.selectedValue);
+                  window.APP.layout.confirmationDialog.deactivate();
+                }).bind(this)
+              })
+
+              .activate();
+
+          else
+
+            this.selectedValue = id;
+
+          this.parent.reset(schemaData);
+        }).bind(this));
+      }
     },
 
     // Returns schema object
@@ -32,61 +70,24 @@ module.exports = {
 
     reset: function(collection) {
       backbone.BaseListView.prototype.reset.call(this, collection);
-
-      // Used to restore previous value when user selects No in confirmation dialog
-      this.selectedValue = this.$(this.options.container).val();
-
-      $.getJSON(this.getSelectedSchema(), (function(schemaData) {
-        this.schemaData = schemaData;
-        this.parent.reset(schemaData);
-      }).bind(this));
-
+      this.setSelected(this.collection.at(0).get('id'));
       return this;
     },
 
     // Update selectbox and trigger change event
-    setSelectedSchema: function(id) {
+    setSelected: function(id) {
       this.$(this.options.container).val(id);
+
+      // Used to restore previous value when user selects No in confirmation dialog
+      this.selectedValue = id;
 
       return new Promise((function(RS, RJ) {
         $.getJSON(this.collection.get(id).get('schema'), (function(schemaData) {
-          var
-            // Keys of entered fields
-            keys = _.keys(this.parent.getFilledValues());
-
           this.schemaData = schemaData;
-
-          // If data can be lost - show confirmation message
-          if(deep.diff(_.pick(schemaData.properties, keys), _.pick(this.parent.layout.form.schema.properties, keys)))
-            return window.APP.layout.confirmationDialog
-              .setMessage('Some data you have entered will be lost. Are you sure you want to change the Data Package Profile?')
-
-              .setCallbacks({
-                yes: (function() {
-                  this.selectedValue = this.$(this.options.container).val();
-                  this.parent.reset(schemaData);
-                  window.APP.layout.confirmationDialog.deactivate();
-                  RS(schemaData);
-                }).bind(this),
-
-                no: (function() {
-                  this.$(this.options.container).val(this.selectedValue);
-                  window.APP.layout.confirmationDialog.deactivate();
-                  RS(schemaData);
-                }).bind(this)
-              })
-
-              .activate();
-
-          else
-            this.selectedValue = this.$(this.options.container).val();
-
           this.parent.reset(schemaData);
           RS(schemaData);
         }).bind(this));
       }).bind(this));
-
-      return this;
     },
 
     tagName: 'select'
