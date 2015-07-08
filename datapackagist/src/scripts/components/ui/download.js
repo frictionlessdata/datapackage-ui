81bbac584015490972aa365ce5c07fd72319a712
nide <nenegoro@gmail.com>
2015-07-08 18:02:24 +0300

    When validating file for download do not validate empty non-required fields. Closes #88.

--- datapackagist/src/scripts/components/ui/download.js
+++ datapackagist/src/scripts/components/ui/download.js
@@ -1,3 +1,4 @@
+var _ = require('underscore');
 var backbone = require('backbone');
 var backboneBase = require('backbone-base');
 var outfile = require('datapackage-outfile');
@@ -7,10 +8,21 @@ var validator = require('datapackage-validate');
 // Download validated datapackage
 module.exports = backbone.BaseView.extend({
   reset: function(descriptor, schema) {
+    var form = window.APP.layout.descriptorEdit.layout.form;
+
+
     this.$el.addClass('disabled');
 
-    validator.validate(window.APP.layout.descriptorEdit.layout.form.getValue(), schema).then((function(R) {
-      if(R.valid)
+    validator.validate(form.getValue(), schema).then((function(R) {
+      var errors = _.filter(R.errors, function(E) {
+        var editor = form.getEditor('root' + E.dataPath.replace(/\//g, '.'));
+        var isRequired = _.contains(editor.parent.schema.required, editor.key);
+
+
+        return isRequired || !isRequired && editor.getValue();
+      });
+
+      if(!errors.length)
         this.$el
           .removeClass('disabled')
 
