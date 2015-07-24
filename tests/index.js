07fb12d134cf492ca7bcd8d1952a17a20aeee918
nide <nenegoro@gmail.com>
2015-07-24 14:54:52 +0300

    From remote test passed (#111)

--- tests/index.js
+++ tests/index.js
@@ -263,8 +263,12 @@ describe('DataPackagist core', function() {
   describe('Ensure From Remote API', function() {
 
     it('a correct CKAN remote results in a data package', function(done) {
-      assert.fail();
-      done();
+      browser.visit('/tabular/from/?source=ckan&url=http%3A%2F%2Fdatahub.io%2Fapi%2Faction%2Fpackage_show%3Fid%3Dpopulation-number-by-governorate-age-group-and-gender-2010-2014&oq=asdf+http%3A%2F%2Fdatahub.io%2Fapi%2Faction%2Fpackage_show%3Fid%3Dpopulation-number-by-governorate-age-group-and-gender-2010-2014&format=json', function() {
+        browser.wait({duration: '10s', element: '[data-schemapath="root.resources.0"]'}).then(function() {
+          browser.assert.element('[data-schemapath="root.resources"] [data-schemapath="root.resources.0"]');
+          done();
+        });
+      });
     });
 
   });
