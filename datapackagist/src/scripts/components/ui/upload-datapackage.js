6e1f28268222919ffd9872d4255e4a3ee94b3d8a
nide <nenegoro@gmail.com>
2015-10-07 17:32:38 +0300

    Upload data package through callback of upload dialog (#149)

--- datapackagist/src/scripts/components/ui/upload-datapackage.js
+++ datapackagist/src/scripts/components/ui/upload-datapackage.js
@@ -8,10 +8,64 @@ var validator = require('datapackage-validate');
 module.exports = backbone.BaseView.extend({
   events: {
     'click': function() {
-      window.APP.layout.uploadDialog.setMessage(
-        'Select data package JSON file from your local drive or enter URL ' +
-        'to download from.'
-      ).activate();
+      window.APP.layout.uploadDialog
+        .setMessage(
+          'Select data package JSON file from your local drive or enter URL ' +
+          'to download from.'
+        )
+
+        .setCallbacks({
+          local: (function(data) {
+            var descriptor;
+            var descriptorEdit;
+            
+
+            window.APP.layout.uploadDialog.deactivate();
+
+            try {
+              descriptor = JSON.parse(data);
+            }
+            catch(E) { }
+
+            descriptorEdit = window.APP.layout.descriptorEdit;
+
+            // If descriptor is broken or If descriptor have field not from schema - reject it
+            if(
+              !_.isObject(descriptor) ||
+
+              _.difference(
+                _.keys(descriptor),
+                _.keys(descriptorEdit.layout.form.schema.properties)
+              ).length
+            ) {
+              window.APP.layout.notificationDialog
+                .setMessage('JSON is invalid')
+                .activate();
+
+              return false;
+            }
+
+            // If there are no changes in current form just apply uploaded
+            // data and leave
+            if(!descriptorEdit.hasChanges()) {
+              this.updateApp(descriptor);
+              return false;
+            }
+
+            // Ask to overwrite changes on current form
+            window.APP.layout.confirmationDialog
+              .setMessage('You have changes. Overwrite?')
+
+              .setCallbacks({yes: (function() {
+                this.updateApp(descriptor);
+                return false;
+              }).bind(this)})
+
+              .activate();
+          }).bind(this)
+        })
+
+        .activate();
 
       return false;
     }
@@ -19,7 +73,13 @@ module.exports = backbone.BaseView.extend({
 
   // Update edit form and download URL
   updateApp: function(descriptor) {
-    this.parent.layout.form.setValue(_.defaults(descriptor, this.parent.layout.form.getValue()));
+    var descriptorEdit = window.APP.layout.descriptorEdit;
+
+
+    descriptorEdit.layout.form.setValue(
+      _.defaults(descriptor, descriptorEdit.layout.form.getValue())
+    );
+
     return this;
   }
 });
