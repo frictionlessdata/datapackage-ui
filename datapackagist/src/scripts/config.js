191f286507228f6b95686c3a61295dd9b440e542
nide <nenegoro@gmail.com>
2015-10-12 20:09:57 +0300

    Load local config file

--- datapackagist/src/scripts/config.js
+++ datapackagist/src/scripts/config.js
@@ -1,7 +1,17 @@
-module.exports = {
-	corsProxyURL: function(url) { return module.exports.host + '/cors-proxy/' + url; },
-	host: 'http://datapackagist.herokuapp.com',
+var _ = require('underscore');
 
-	// How many rows of CSV to process when parsing it into object and infering schema
-	maxCSVRows: 100
-};
+
+try {
+  var localConfig = require('./config-local');
+} catch(E) {
+  var localConfig = {};
+}
+
+
+module.exports = _.extend({
+  corsProxyURL: function(url) { return module.exports.host + '/cors-proxy/' + url; },
+  host: 'http://datapackagist.herokuapp.com',
+
+  // How many rows of CSV to process when parsing it into object and infering schema
+  maxCSVRows: 100
+}, localConfig);
