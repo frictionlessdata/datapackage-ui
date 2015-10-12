3a346e21ad0b3963c65d73b852189a740194bb5a
nide <nenegoro@gmail.com>
2015-10-12 19:06:40 +0300

    Implement local CORS proxy (#148)

--- datapackagist/app.js
+++ datapackagist/app.js
@@ -1,7 +1,22 @@
+var _ = require('underscore');
 var express = require('express');
 var app = express();
+var superagent = require('superagent-bluebird-promise');
 
 
 app.use(express.static(__dirname + '/dist'));
-app.get('*', function(request, response){ response.sendFile(__dirname + '/dist/index.html'); });
+
+app.get('/cors-proxy/*', function(request, response) {
+  superagent.get(request.params[0] + (
+    !_.isEmpty(request.query)
+    ? ('?' + _.chain(request.query).pairs().map(function(P) { return P.join('=') }).value().join('&'))
+    : ''
+  )).then(function(data) {
+    response
+      .set({'content-type': data.header['content-type']})
+      .send(data.text);
+  });
+});
+
+app.get('*', function(request, response) { response.sendFile(__dirname + '/dist/index.html'); });
 module.exports = app;
